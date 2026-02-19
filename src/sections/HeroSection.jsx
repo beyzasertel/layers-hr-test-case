720e2c4635cba4d73df073848699b2a6f6b49672
beyzasertel <beyza786@outlook.com>
2026-02-19 23:36:26 +0300

    hero section completed

---
+++ src/sections/HeroSection.jsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Button from "@/components/common/Button";
+import SearchBar from "@/components/common/Searchbar";
+import { useMessages, useLocale, useTranslations } from "next-intl";
+
+export default function HeroSection() {
+  const locale = useLocale();
+  const messages = useMessages(); // ✅ provider yoksa burada patlar / boş gelir
+  const t = useTranslations("common");
+
+  return (
+    <div className="bg-hero bg-cover py-4">
+      <div className="container w-full flex justify-end items-center gap-2 md:gap-4 px-2 md:px-0 py-4">
+        <Button>{t("sign_up")}</Button>
+        <Button>{t("sign_in")}</Button>
+      </div>
+
+      <div className="flex items-center flex-col gap-2 p-12">
+        <img src="/images/logo-find-developer.svg" alt="logo" />
+        <img src="/images/logo-find-teammate.svg" alt="logo" />
+      </div>
+
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col md:flex-row items-center md:items-start justify-center gap-4">
+          <div className="flex-1 min-w-0">
+            <SearchBar />
+          </div>
+
+          <div className="flex flex-col items-center">
+            <Button variant="image">&nbsp;</Button>
+
+            <a href="#" className="mt-2 text-sm text-white underline">
+              {t("advanced_search")}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
