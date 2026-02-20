5cd64272050b4ea8716be6109404c83c93505ad3
beyzasertel <beyza786@outlook.com>
2026-02-20 09:03:17 +0300

    Language section added to footer

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,29 +1,51 @@
 "use client";
 
 import { useLocale } from "next-intl";
-import { Link, usePathname } from "@/i18n/navigation";
+import { usePathname, useRouter } from "@/i18n/navigation";
+
+import { Select, MenuItem } from "@mui/material";
 
 export default function LanguageSwitcher() {
   const locale = useLocale();
   const pathname = usePathname();
+  const router = useRouter();
+
+  const handleChange = (event) => {
+    const newLocale = event.target.value;
+
+    router.push(pathname, { locale: newLocale });
+  };
 
   return (
-    <div className="flex gap-2">
-      <Link
-        href={pathname}
-        locale="tr"
-        className={`px-3 py-2 rounded border ${locale === "tr" ? "font-bold" : ""}`}
-      >
-        TR
-      </Link>
+    <Select
+      value={locale}
+      onChange={handleChange}
+      variant="standard"
+      disableUnderline
+      sx={{
+        color: "white",
+        fontSize: "14px",
+        fontWeight: 500,
 
-      <Link
-        href={pathname}
-        locale="en"
-        className={`px-3 py-2 rounded border ${locale === "en" ? "font-bold" : ""}`}
-      >
-        EN
-      </Link>
-    </div>
+        "&:before": {
+          borderBottom: "none",
+        },
+        "&:after": {
+          borderBottom: "none",
+        },
+
+        "& .MuiSvgIcon-root": {
+          color: "white",
+          fontSize: "20px",
+        },
+
+        "& .MuiSelect-select": {
+          paddingRight: "24px !important",
+        },
+      }}
+    >
+      <MenuItem value="tr">Türkçe</MenuItem>
+      <MenuItem value="en">English</MenuItem>
+    </Select>
   );
 }
